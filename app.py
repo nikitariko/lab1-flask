c0515c21dd02630dd0b82ddce23811bf3cf16383
Nikita-Herasymchuk <nikitaherasimchuk2005@gmail.com>
2025-06-22 15:51:35 +0200

    flask app for Render

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Привіт, це моя Flask-програма на Render!'
+    return 'Hello, this is a Flask app on Render!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
